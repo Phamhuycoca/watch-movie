0099a8302d3e45732d35c38c41e232aa290c4808
Phamhuycoca <phamkhachuy240702@gmail.com>
2024-12-29 03:15:31 +0700

    update lib

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,8 +1,11 @@
+import { Footer } from "~/components/Footer";
+import { Header } from "~/components/Header";
 
 function App() {
   return (
     <div className="App">
-     <h1>ok</h1>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
