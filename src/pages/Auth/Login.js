75e783bddccbd81028d85a1a5f5aaed430c8c1fd
Phamhuycoca <phamkhachuy240702@gmail.com>
2025-01-01 17:05:13 +0700

    ud login

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input, Row } from 'antd';
-import breadcrumb_bg from './../../assets/pricing_bg.jpg';
+import { useStyleRegister } from '@ant-design/cssinjs';
+import { Button, Checkbox, Col, Form, Image, Input, Row, theme } from 'antd';
+import pricing_bg from './../../assets/pricing_bg.jpg';
+import classNames from 'classnames';
+import { genLogin } from './genLogin.style';
+import { Link } from 'react-router-dom';
+import logo from './../../assets/logo.png';
+
+const prefixCls = 'custom-login';
 
 const onFinish = (values) => {
     console.log('Success:', values);
@@ -9,61 +16,135 @@ const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
 };
 
-const Login = () => (
-    <Row
-        justify={'center'}
-        style={{
-            height: '100%',
-            minHeight: '912px',
+const Login = () => {
+    const { token } = theme.useToken(); // Di chuyển useToken vào trong component
+    const infoStyle = { token, path: [prefixCls] };
 
-            alignItems: 'center',
-            backgroundImage: `url(${breadcrumb_bg})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-        }}
-    >
-        <Form
-            layout="vertical"
-            name="basic"
-            style={{
-                maxWidth: 400,
-                width: '100%',
-                minWidth: 400,
-                display: 'flex',
-                flexDirection: 'column',
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label={<span style={{ color: '#e0d505' }}>Tên đăng nhập</span>}
-                name="username"
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                label={<span style={{ color: '#e0d505' }}>Mật khẩu</span>}
-                name="password"
-            >
-                <Input.Password />
-            </Form.Item>
-            <Form.Item name="remember" valuePropName="checked" label={null}>
-                <Checkbox>Ghi nhớ thông tin</Checkbox>
-            </Form.Item>
+    const wrapSSR = useStyleRegister(infoStyle, () =>
+        genLogin(prefixCls, infoStyle.token),
+    );
 
-            <Form.Item label={null}>
-                <Row justify={'center'}>
-                    <Button type="primary" htmlType="submit">
-                        Đăng nhập
-                    </Button>
-                </Row>
-            </Form.Item>
-        </Form>
-    </Row>
-);
+    return wrapSSR(
+        <div className={classNames(prefixCls, infoStyle.hashId)}>
+            <Row
+                justify={'center'}
+                style={{
+                    height: '100%',
+                    minHeight: '912px',
+                    alignItems: 'center',
+                    backgroundImage: `url(${pricing_bg})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                }}
+            >
+                <Col>
+                    <Row justify={'center'} className="mb-4">
+                        <Image
+                            src={logo}
+                            height={50}
+                            width={200}
+                            preview={false}
+                        />
+                    </Row>
+                    <Row>
+                        <Form
+                            layout="vertical"
+                            name="basic"
+                            style={{
+                                maxWidth: 400,
+                                width: '100%',
+                                minWidth: 400,
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
+                        >
+                            <Form.Item
+                                label={
+                                    <span
+                                        style={{
+                                            color: '#e0d505',
+                                            fontSize: '18px',
+                                        }}
+                                    >
+                                        Tên đăng nhập
+                                    </span>
+                                }
+                                name="username"
+                            >
+                                <Input
+                                    variant="borderless"
+                                    size="large"
+                                    placeholder="Nhập thông tin"
+                                    style={{
+                                        borderBottom: '1px solid #e0d505',
+                                        borderRadius: '0px',
+                                        caretColor: '#e0d505',
+                                        color: '#fff',
+                                        fontSize: '18px',
+                                        lineHeight: '40px',
+                                    }}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                label={
+                                    <span
+                                        style={{
+                                            color: '#e0d505',
+                                            fontSize: '18px',
+                                        }}
+                                    >
+                                        Mật khẩu
+                                    </span>
+                                }
+                                name="password"
+                            >
+                                <Input.Password
+                                    variant="borderless"
+                                    size="large"
+                                    placeholder="Nhập thông tin"
+                                    style={{
+                                        borderBottom: '1px solid #e0d505',
+                                        borderRadius: '0px',
+                                        caretColor: '#e0d505',
+                                        color: '#fff',
+                                        fontSize: '18px',
+                                        lineHeight: '40px',
+                                    }}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="remember"
+                                valuePropName="checked"
+                                label={null}
+                            >
+                                <Row justify={'space-between'}>
+                                    <Checkbox style={{ color: '#fff' }}>
+                                        Ghi nhớ thông tin
+                                    </Checkbox>
+                                    <Link style={{ color: '#fff' }} to="/">
+                                        Bạn chưa có tài khoản?
+                                    </Link>
+                                </Row>
+                            </Form.Item>
+
+                            <Form.Item label={null}>
+                                <Row justify={'center'}>
+                                    <Button htmlType="submit">Đăng nhập</Button>
+                                </Row>
+                            </Form.Item>
+                        </Form>
+                    </Row>
+                </Col>
+            </Row>
+        </div>,
+    );
+};
+
 export default Login;
