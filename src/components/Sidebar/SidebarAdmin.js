26f65b8424bed10a5c949ef246a3e7bdc7be0448
Phamhuycoca <phamkhachuy240702@gmail.com>
2025-01-14 23:38:08 +0700

    update

--- src/components/Sidebar/SidebarAdmin.js
+++ src/components/Sidebar/SidebarAdmin.js
@@ -1,9 +1,119 @@
 import { Layout, Menu, theme } from 'antd';
+import {
+    AppstoreOutlined,
+    MailOutlined,
+    SettingOutlined,
+} from '@ant-design/icons';
 const { Sider } = Layout;
-const items = new Array(100).fill(null).map((_, index) => ({
-    key: String(index + 1),
-    label: `nav ${index + 1}`,
-}));
+const items = [
+    {
+        key: 'sub1',
+        label: 'Navigation One',
+        icon: <MailOutlined />,
+        children: [
+            {
+                key: 'g1',
+                label: 'Item 1',
+                type: 'group',
+                children: [
+                    {
+                        key: '1',
+                        label: 'Option 1',
+                    },
+                    {
+                        key: '2',
+                        label: 'Option 2',
+                    },
+                ],
+            },
+            {
+                key: 'g2',
+                label: 'Item 2',
+                type: 'group',
+                children: [
+                    {
+                        key: '3',
+                        label: 'Option 3',
+                    },
+                    {
+                        key: '4',
+                        label: 'Option 4',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            {
+                key: '5',
+                label: 'Option 5',
+            },
+            {
+                key: '6',
+                label: 'Option 6',
+            },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    {
+                        key: '7',
+                        label: 'Option 7',
+                    },
+                    {
+                        key: '8',
+                        label: 'Option 8',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        type: 'divider',
+    },
+    {
+        key: 'sub4',
+        label: 'Navigation Three',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                key: '9',
+                label: 'Option 9',
+            },
+            {
+                key: '10',
+                label: 'Option 10',
+            },
+            {
+                key: '11',
+                label: 'Option 11',
+            },
+            {
+                key: '12',
+                label: 'Option 12',
+            },
+        ],
+    },
+    {
+        key: 'grp',
+        label: 'Group',
+        type: 'group',
+        children: [
+            {
+                key: '13',
+                label: 'Option 13',
+            },
+            {
+                key: '14',
+                label: 'Option 14',
+            },
+        ],
+    },
+];
 function SidebarAdmin() {
     const {
         token: { colorBgContainer },
